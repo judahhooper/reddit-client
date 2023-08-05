e60a18e45a4dc5551a6c74e47ebeb77af4026a99
Judah Hooper <judahhooper@icloud.com>
2023-08-05 13:26:09 +0100

    sidenav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SideNav from "./SideNav";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <SideNav></SideNav>
     </div>
   );
 }
