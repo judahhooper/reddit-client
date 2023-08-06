c7e93e27045d729792b4f18790047a8ee49de9a2
Judah Hooper <judahhooper@icloud.com>
2023-08-06 14:32:22 +0100

    add boilerplate for postitem and post components

--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,7 @@ export default function Main () {
     return (
         <div className="main">
             <Header/>
-            <Posts/>
+        
         </div>
     )
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,1 +1,15 @@
 import React from "react";
+import PostItem from "./PostItem"
+
+export default function Posts () {
+    const posts = [
+        { upvote: '634', image: '', title:'lorem', user: 'user1', subreddit:'example', commentcounts: '221'}
+    ]
+    return(
+        <div className="posts">
+            {posts.map(post=>(
+                <PostItem post={post}/>
+            ))}
+        </div>
+    )
+}
